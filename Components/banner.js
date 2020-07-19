b4442cc12cf02c5eab88c4c4550b60e39e25128d
Andrew Tran <andtran@umass.edu>
2020-07-19 19:26:13 -0400

    Added some sample pictures changed background color on banner Removed stepper below banner

--- Components/banner.js
+++ Components/banner.js
@@ -1,66 +1,53 @@
 import React from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import MobileStepper from '@material-ui/core/MobileStepper';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button'
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import SwipeableViews from 'react-swipeable-views';
 import { autoPlay } from 'react-swipeable-views-utils';
 import Box from '@material-ui/core/Box';
 
-
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 // to update banner on home page, change tutorialSteps properties
 const tutorialSteps = [
   {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
+    label: 'The Fellas',
     imgPath:
-      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+      '/TheFellas.jpg',
 	//href:
   },
   {
-    label: 'Bird',
+    label: 'Lunabotics',
     imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
-	  //href:
-  },
-  {
-    label: 'Bali, Indonesia',
-    imgPath:
-      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
-	  //href:
-  },
-  {
-    label: 'NeONBRAND Digital Marketing, Las Vegas, United States',
-    imgPath:
-      'https://images.unsplash.com/photo-1518732714860-b62714ce0c59?auto=format&fit=crop&w=400&h=250&q=60',
-	  //href:
-  },
-  {
-    label: 'Goč, Serbia',
-    imgPath:
-      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+      '/LunaboticsLogo.jpg',
 	  //href:
   },
 ];
 
 const useStyles = makeStyles((theme) => ({
   root: {
-	maxHeight: 500,
+	maxHeight: 600,
     flexGrow: 1,
+	background: '#2C2C2C'
   },
   stepper: {
-	padding: '2px',
 	background: "#9A1E1E",
+	width: '60%',
+	alignItems:'center',
+	justifyContent:'center'
+  },
+  images: {
+	  display:'flex',
+	  alignItems:'center',
+	  justifyContent:'center'
   },
   img: {
     height: 500,
     display: 'block',
     overflow: 'hidden',
-    width: '100%',
+	padding: '0 0 20px'
   },
 }));
 
@@ -84,7 +71,6 @@ function SwipeableTextMobileStepper() {
 
   return (
     <div className={classes.root}>
-
       <AutoPlaySwipeableViews
         axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
         index={activeStep}
@@ -94,23 +80,14 @@ function SwipeableTextMobileStepper() {
 		elevation={2}
       >
         {tutorialSteps.map((step, index) => (
-          <div key={step.label}>
-            {Math.abs(activeStep - index) <= 2 ? (
-              <img className={classes.img} src={step.imgPath} alt={step.label} />
-            ) : null}
+          <div key={step.label} className={classes.images}>
+	            {Math.abs(activeStep - index) <= 2 ? (
+	              <img className={classes.img} src={step.imgPath} alt={step.label} />
+	            ) : null}
           </div>
         ))}
       </AutoPlaySwipeableViews>
-	  <Box boxShadow={3}>
-		  <MobileStepper
-		  	className={classes.stepper}
-	        steps={maxSteps}
-	        position="static"
-	        variant="dots"
-	        activeStep={activeStep}
-			elevation={24}
-	      />
-	  </Box>
+
     </div>
   );
 }
