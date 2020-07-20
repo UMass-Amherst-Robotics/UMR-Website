e50db9687f9ee4d788d0991a126a3c3ad78ca60e
Andrew Tran <andtran@umass.edu>
2020-07-19 23:38:28 -0400

    Added link to gofundme when clicking DONATE on navbar

--- Components/navbar.js
+++ Components/navbar.js
@@ -6,7 +6,6 @@ import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
 import Toolbar from '@material-ui/core/Toolbar'
 import Link from 'next/link'
-
 const useStyles = makeStyles((theme) => ({
 	root: {
 		position: 'relative',
@@ -43,11 +42,11 @@ const Navbar = () => {
       	<AppBar elevation={0} position="sticky" className={classes.menubar}>
         	<Toolbar>
 						<a href='/'><img src="/umr-logo-white.png" alt="logo" className={classes.logo} href='/' /></a>
-            	<Button className={classes.menu_button} color="inherit" href='/'>HOME</Button>
-              <Button className={classes.menu_button} color="inherit" href='/about-us'>ABOUT US</Button>
-              <Button className={classes.menu_button} color="inherit" href='/contact'>CONTACT</Button>
-              <Button className={classes.menu_button} color="inherit" href='/sponsors'>SPONSORS</Button>
-              <Button className={classes.menu_button} colot="inhereit" href='/donate'>DONATE</Button>
+							<Button className={classes.menu_button} color="inherit" href='/'>HOME</Button>
+							<Button className={classes.menu_button} color="inherit" href='/about-us'>ABOUT US</Button>
+							<Button className={classes.menu_button} color="inherit" href='/contact'>CONTACT</Button>
+							<Link href='/sponsors'><Button className={classes.menu_button} color="inherit">SPONSORS</Button></Link>
+							<Button className={classes.menu_button} colot="inhereit" href='https://www.gofundme.com/f/umass-robotics?utm_source=customer&utm_medium=copy_link&utm_campaign=p_cf+share-flow-1'>DONATE</Button>
           </Toolbar>
 				</AppBar>
 			</div>
