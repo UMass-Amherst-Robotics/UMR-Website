ce7262044f53af8326277f6129ddcd8a4c937952
sdubois18 <samdubois18@icloud.com>
2020-07-28 17:51:07 -0400

    Added code to support grids in the contact page. got started on the layout

--- pages/contact.js
+++ pages/contact.js
@@ -1,14 +1,57 @@
 import React from 'react';
 import Navbar from './../Components/navbar.js'
 import Body from './../Components/body.js'
-import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField'
+import FormControl from '@material-ui/core/FormControl'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: 40,
+    flexGrow: 1,
+  },
+  title:{
+    fontWeight: 'bold',
+    fontSize: 60,
+    textAlign: 'left',
+    color: '#9A1E1E',
+  },
+  textField: {
+    width: '25ch',
+  },
+  margin: {
+    margin: theme.spacing(1),
+  }
+}));
 
 export default function Contact() {
+  const classes = useStyles();
+
   return (
     <body style={{margin: "0"}}>
       <div>
         <Navbar/>
-          <h1> Contact </h1>
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+        >
+          <div className={classes.root}>
+            <Grid item xs>
+              <Typography className={classes.title} variant = "h1" gutterBottom>Contact</Typography>
+            </Grid>
+            <Grid item xs>
+              <form className={classes.root} noValidate autoComplete="off">
+                <FormControl fullWidth className={classes.margin}>
+                  <TextField className={classes.textField} id="standard-basic" label="Standard" />
+                </FormControl>
+              </form>
+            </Grid>
+          </div>
+        </Grid>
       </div>
     </body>
   )
