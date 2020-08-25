f35753707c15b8ec87706a8c86da77e606860318
Andrew Tran <andtran@umass.edu>
2020-08-25 16:54:24 -0400

    testing new donator route

--- Components/sponsors/golddonators.js
+++ Components/sponsors/golddonators.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import golds from './data/gold_donators.json'
+import React, {useState} from 'react';
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
 import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Box'
+import axios from 'axios';
 
 export default function GoldSponsors() {
   const xs = 12;
   const sm = 6;
+
+  const [golds, setGolds] = useState([])
+
+  async function getGold(){
+    await axios.get('http://127.0.0.1:5000/donators/').then(
+      (res) => {
+        let list = res.data['gold']
+        setGolds(list)
+      }
+    )
+  }
+
+  getGold()
+
   return (
     <div className='gold-container'>
       <Grid container>
