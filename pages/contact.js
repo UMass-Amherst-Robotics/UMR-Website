cd29690db99dd82cada29f0a40da5a65ce96b943
Andrew Tran <andtran@umass.edu>
2020-08-25 02:07:58 -0400

    Contact Page Validation Added form validation  - All fields must be filled before email can be sent  - must be a valid email address  - alerts are shown if message was sent or if it was not received

--- pages/contact.js
+++ pages/contact.js
@@ -18,14 +18,14 @@ const useStyles = makeStyles((theme) => ({
   button:{
     marginLeft: '42%',
     marginBottom: 50,
-    padding: 15,  
+    padding: 15,
     backgroundColor: '#312927',
     color: '#FFFFFF',
   },
   badButton:{
     marginLeft: '42%',
     marginBottom: 50,
-    padding: 15,  
+    padding: 15,
     backgroundColor: '#50e78e',
     color: '#303030',
   },
@@ -49,23 +49,49 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Contact() {
   const classes = useStyles();
-  const [submitted, setSubmitted] = useState(false);
-  const [content, setContent] = useState({fistName: '', lastName: '', email: '', subject: '', message: ''})
+  const [submitted, setSubmitted] = useState(null);
+  const [content, setContent] = useState({firstName: '', lastName: '', email: '', subject: '', message: ''})
 
   // Description: Function that will make post request to server with contact information to then be exported to the umassrobotics email address.
-  async function submitEmail(data){
+  function submitEmail(data){
       const json = JSON.stringify(data);
-      const res = await axios.post('http://127.0.0.1:5000/data/', json, {
+      const res = axios.post('http://127.0.0.1:5000/contact/', json, {
         headers: {
           // Overwrite Axios's automatically set Content-Type
           'Content-Type': 'application/json'
         }
-      });
+      }).then((repsonse) => {
+        setSubmitted(true);
+        alert('Message received. Thank you! We will try to get back to you as soon as possible.')
+      }, (error) => {
+        setSubmitted(false);
+        alert('Message not received. Please try again later.')
+      })
   }
 
   function resetForm(){
     setContent({firstName: '', lastName: '', email: '',subject:'', message: ''})
-}
+  }
+
+  // Description: checks if all fields are correctly written first before sending request to email server
+  function handleSubmit(content){
+    if (content['firstName'] && content['lastName'] && content['email'] && content['subject'] && content['message'] && ValidateEmail(content['email'])){
+      submitEmail(content)
+    }
+    else {
+      if(!ValidateEmail(content['email'])){
+        return alert('Please enter a valid email adress you fucking dumbass')
+      }
+      return alert('All fields must be filled.')
+    }
+  }
+
+  function ValidateEmail(mail){
+    if (mail.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)){
+      return (true)
+    }
+    return (false)
+  }
 
   return (
     <body style={{margin: "0"}}>
@@ -75,17 +101,17 @@ export default function Contact() {
             <Typography className={classes.title} variant = "h1" gutterBottom>Contact</Typography>
             <form noValidate autoComplete="off">
               <div className={classes.margin}>
-                <TextField 
-                  className={classes.textField} 
-                  id="standard-full-width" 
+                <TextField
+                  className={classes.textField}
+                  id="standard-full-width"
                   label="First Name"
                   onChange={(e) => {setContent({firstName: e.target.value, lastName: content.lastName, email: content.email, subject: content.subject, message: content.message})}}
                 />
-                <TextField 
-                  fullWidth 
-                  className={classes.textField} 
-                  id="standard-full-width" 
-                  label="Last Name" 
+                <TextField
+                  fullWidth
+                  className={classes.textField}
+                  id="standard-full-width"
+                  label="Last Name"
                   onChange={(e) => {setContent({firstName: content.firstName, lastName: e.target.value, email: content.email, subject: content.subject, message: content.message})}}
                 />
               </div>
@@ -112,10 +138,10 @@ export default function Contact() {
                 onChange={(e) => {setContent({firstName: content.firstName, lastName: content.lastName, email: content.email, subject: content.subject, message: e.target.value})}}
               />
             </form>
-            <Button className={submitted ? classes.badButton : classes.button} variant="contained" onClick={() => {submitEmail(content)}}> 
+            <Button className={submitted ? classes.badButton : classes.button} variant="contained" onClick={() => {handleSubmit(content)}}>
               Submit
             </Button>
-        </div>
+          </div>
         <Footer />
       </div>
     </body>
