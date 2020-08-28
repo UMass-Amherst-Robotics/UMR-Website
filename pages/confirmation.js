c772bd1c35c3a5e744e611808f1d18abe93d4a4c
Andrew Tran <andtran@umass.edu>
2020-08-28 11:15:46 -0400

    Fixed the fucking shitty as page

--- pages/confirmation.js
+++ pages/confirmation.js
@@ -2,39 +2,37 @@ import React from 'react';
 import Navbar from './../Components/navbar'
 import Footer from '../Components/Footer/footer'
 import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box'
+import Grid from '@material-ui/core/Box'
 import { createMuiTheme, makeStyles } from '@material-ui/core/styles';
 
-const theme = createMuiTheme({
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 700,
-      md: 875,
-      lg: 1280,
-      xl: 1920
-    }
-  }
-});
-
 const useStyles = makeStyles((theme) => ({
+  whitespace: {
+    minHeight: "100vh",
+    position: "relative",
+  },
   root: {
     paddingTop: 50,
-    width: '50%',
+    width: '100%',
     maxWidth: '800px',
     margin: 'auto',
   },
   title:{
     fontWeight: 'bold',
     fontSize: 60,
+    [theme.breakpoints.down('xs')]: {
+      fontSize: 35,
+    },
     textAlign: 'left',
     color: '#9A1E1E',
     marginBottom: 20,
   },
 
   text:{
-    fontSize: '1.75rem',
-    textAlign: 'left'
+    fontSize: 30,
+    textAlign: 'left',
+    [theme.breakpoints.down('xs')]: {
+      fontSize: 25,
+    },
   },
   footer: {
     position: "absolute",
@@ -47,16 +45,20 @@ export default function(){
   const classes = useStyles();
 
   return(
-    <body style={{margin: "0"}}>
+    <body style={{width: '100%', margin: "0"}}>
       <div>
-        <Navbar/>
+      <Grid container direction='column' justify='center' alignItems='center'>
+        <Grid item>
+          <Navbar/>
           <div className={classes.root}>
-            <Typography className={classes.title} variant = "h1" gutterBottom>Confirmation</Typography>
+            <Typography className={classes.title} gutterBottom>Confirmation</Typography>
             <Typography className={classes.text} variant='h5' gutterBottom>Thank you for your interest with UMass Robotics. We have received your application. Please check your email for confirmation and follow the instructions.</Typography>
           </div>
-        <Box className={classes.footer}>
+          </Grid>
+        <Grid item className={classes.footer}>
           <Footer/>
-        </Box>
+        </Grid>
+        </Grid>
       </div>
     </body>
   )
