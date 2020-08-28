2cda746ec4dfdf33f9b202037083ab38c386bcac
Michael Brauninger <mbrauninger@umass.edu>
2020-08-28 18:21:05 -0400

    Icons now individually assignable on cards, and buttons now centered on form pages

--- pages/meet-the-team.js
+++ pages/meet-the-team.js
@@ -2,18 +2,15 @@ import React from 'react';
 import Navbar from './../Components/navbar.js'
 import Body from './../Components/home/body.js'
 import MediaCard from './../Components/Cards/card.js'
-import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme, makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-import { useTheme } from '@material-ui/core/styles';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Typography from '@material-ui/core/Typography';
-import ListItem from '@material-ui/core/ListItem';
 import { MuiThemeProvider} from '@material-ui/core/styles';
-import { Divider } from '@material-ui/core';
-import Container from '@material-ui/core/Container';
 import Footer from '../Components/Footer/footer.js'
-import { positions } from '@material-ui/system';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import CardIcon from './../Components/CardIcon.js'
 
 const useStyles = makeStyles({
   root: {
@@ -22,6 +19,9 @@ const useStyles = makeStyles({
   },
   header: {
     color: "#9A1E1E"
+  },
+  icon: {
+    fontSize: 50,
   }
 });
 
@@ -51,32 +51,39 @@ const theme = createMuiTheme({
   }
 });
 
-let execs = [
-  {name: "Sam Dubois", image: "/Profiles/SamPicture.png", execRole: "President", major: "C.S. and Computer Engineering", work: "Electronics, programming", hobbies: "Rock climbing, machining", diningHall: "Berk", linkedIn: "https://www.linkedin.com/in/samuel-dubois/", gitHub: "https://github.com/sdubois18"},
-  {name: "Sean Flanagan", image: "/Profiles/SeanFlanPicture.png", execRole: "Vice-President", major: "Mechanical Engineering", work: "Mechanical design, analysis", hobbies: "Spikeball, volleyball, table tennis", diningHall: "Hamp", linkedIn: "https://www.linkedin.com/in/sean-flanagan-997480157/", gitHub: "https://github.com/smflanagan"},
-  {name: "Dom Locurto", image: "/Profiles/DomPicture.png", execRole: "Treasurer", major: "Mechanical Engineering", work: "Mechanical design, analysis", hobbies: "Ultimate frisbee, cooking, gaming", diningHall: "Hamp", linkedIn: "http://linkedin.com/in/dominic-locurto-888103194", gitHub: "https://github.com/DomLocurto"},
-  {name: "Alex Klinkhamer", image: "/Profiles/AlexPicture.png", execRole: "Secretary", major: "Mechanical Engineering", work: "Mechanical design, analysis", hobbies: "3D printing, designing, CAD, running", diningHall: "Worcester", gitHub: "https://github.com/Aklinkhamer"},
-  {name: "Jagath Jai Kumar", image: "/Profiles/JagathPicture.png", execRole: "Cyber Forensics Research Fellow", major: "C.S. Graduate", work: "General advising and mentorship", hobbies: "Rock climbing, 3D printing", diningHall: "Berk", linkedIn: "https://www.linkedin.com/in/jagath-jai-kumar/", gitHub: "https://github.com/jagath-jaikumar"},
-  {name: "Adam Rivelli", image: "/Profiles/AdamPicture.png", execRole: "C.S. Master's Student", major: "C.S. and Mechanical Engineering Graduate", work: "Mechanical engineering mentorship", hobbies: "Rock climbing, running, hiking", diningHall: "Worcester", linkedIn: "https://www.linkedin.com/in/adam-rivelli", gitHub: "https://github.com/AdamRivelli"}
-
-
-]
-
-let members = [
-    {name: "Mike Brauninger", image: "/Profiles/MikePic.png", major: "Computer Engineering", work: "Electronics, programming, website design", hobbies: "Frisbee, guitar, fishing", diningHall: "Berk", linkedIn: "https://www.linkedin.com/in/mbrauninger/", gitHub: "https://github.com/mbrauninger"},
-    {name: "Andrew Tran", image: "/Profiles/AndrewPicture.png", major: "Computer Engineering", work: "Electronics, programming, website design", hobbies: "Cooking, personal coding projects", diningHall: "Worcester", linkedIn: "https://www.linkedin.com/in/umass-andrew-tran/", gitHub: "https://github.com/andrewtran10"},
-    {name: "Aidan Downey", image: "/Profiles/AidanPicture.png", major: "Mechanical Engineering", work: "Mechanical design, analysis", hobbies: "Sailing, guitar, gain time", diningHall: "Berk", gitHub: "https://github.com/apdowney"},
-    {name: "Kyle O'Connell", image: "/Profiles/KylePicture.png", major: "Mechanical Engineering", work: "Mechanical design, analysis", hobbies: "Frisbee, music", diningHall: "Hamp", linkedIn: "https://www.linkedin.com/in/kyle-o-connell-763273175/", gitHub: "https://github.com/kyleo9918"},
-    {name: "Andy Gutierrez", image: "/Profiles/GutiPic.png", major: "Computer Engineering", work: "Electronics, programming, website design", hobbies: "Piano, guitar, squash", diningHall: "Berk", linkedIn: "https://www.linkedin.com/in/andres-gutierrez-8758581a2/", gitHub: "https://github.com/Andy-Guti"},
-    {name: "Dillan Wilson", image: "/Profiles/DillanPicture.png", major: "Mechanical Engineering", work: "Chassis design", hobbies: "Running, hockey, table tennis", diningHall: "Hamp", linkedIn: "https://www.linkedin.com/in/dillan-wilson-2a2116175/", gitHub: "https://github.com/dillan-wilson"}
-    ]
-
 export default function Team() {
   const classes = useStyles()
   const spacing = 5
   const xSmall = 12
   const small = 6
   const medium = 4
+
+  const linkedInIcon =  <LinkedInIcon className={classes.icon}></LinkedInIcon> 
+  const gitHubIcon =   <GitHubIcon color="secondary" className={classes.icon}/>
+  
+  function icon(link, icon) {
+    return <CardIcon link={link} icon={icon} />
+  }
+
+  let execs = [
+    {name: "Sam Dubois", image: "/Profiles/SamPicture.png", execRole: "President", major: "C.S. and Computer Engineering", work: "Electronics, programming", hobbies: "Rock climbing, machining", diningHall: "Berk", linkedInIcon: icon("https://www.linkedin.com/in/samuel-dubois/", linkedInIcon), gitHubIcon: icon('https://github.com/sdubois18', gitHubIcon)},
+    {name: "Sean Flanagan", image: "/Profiles/SeanFlanPicture.png", execRole: "Vice-President", major: "Mechanical Engineering", work: "Mechanical design, analysis", hobbies: "Spikeball, volleyball, table tennis", diningHall: "Hamp", linkedInIcon: icon("https://www.linkedin.com/in/sean-flanagan-997480157/", linkedInIcon), gitHubIcon: icon("https://github.com/smflanagan", gitHubIcon)},
+    {name: "Dom Locurto", image: "/Profiles/DomPicture.png", execRole: "Treasurer", major: "Mechanical Engineering", work: "Mechanical design, analysis", hobbies: "Ultimate frisbee, cooking, gaming", diningHall: "Hamp", linkedInIcon: icon("http://linkedin.com/in/dominic-locurto-888103194", linkedInIcon), gitHubIcon: icon("https://github.com/DomLocurto", gitHubIcon)},
+    {name: "Alex Klinkhamer", image: "/Profiles/AlexPicture.png", execRole: "Secretary", major: "Mechanical Engineering", work: "Mechanical design, analysis", hobbies: "3D printing, designing, CAD, running", diningHall: "Worcester", gitHubIcon: icon("https://github.com/Aklinkhamer", gitHubIcon)},
+    {name: "Jagath Jai Kumar", image: "/Profiles/JagathPicture.png", execRole: "Cyber Forensics Research Fellow", major: "C.S. Graduate", work: "General advising and mentorship", hobbies: "Rock climbing, 3D printing", diningHall: "Berk", linkedInIcon: icon("https://www.linkedin.com/in/jagath-jai-kumar/", linkedInIcon), gitHubIcon: icon("https://github.com/jagath-jaikumar", gitHubIcon)},
+    {name: "Adam Rivelli", image: "/Profiles/AdamPicture.png", execRole: "C.S. Master's Student", major: "C.S. and Mechanical Engineering Graduate", work: "Mechanical engineering mentorship", hobbies: "Rock climbing, running, hiking", diningHall: "Worcester", linkedInIcon: icon("https://www.linkedin.com/in/adam-rivelli", linkedInIcon), gitHubIcon: icon("https://github.com/AdamRivelli", gitHubIcon)}
+  ]
+
+  let members = [
+    {name: "Mike Brauninger", image: "/Profiles/MikePic.png", major: "Computer Engineering", work: "Electronics, programming, website design", hobbies: "Frisbee, guitar, fishing", diningHall: "Berk", linkedInIcon: icon("https://www.linkedin.com/in/mbrauninger/", linkedInIcon), gitHubIcon: icon("https://github.com/mbrauninger", gitHubIcon)},
+    {name: "Andrew Tran", image: "/Profiles/AndrewPicture.png", major: "Computer Engineering", work: "Electronics, programming, website design", hobbies: "Cooking, personal coding projects", diningHall: "Worcester", linkedInIcon: icon("https://www.linkedin.com/in/umass-andrew-tran/", linkedInIcon), gitHubIcon: icon("https://github.com/andrewtran10", gitHubIcon)},
+    {name: "Aidan Downey", image: "/Profiles/AidanPicture.png", major: "Mechanical Engineering", work: "Mechanical design, analysis", hobbies: "Sailing, guitar, gain time", diningHall: "Berk", gitHubIcon: icon("https://github.com/apdowney", gitHubIcon)},
+    {name: "Kyle O'Connell", image: "/Profiles/KylePicture.png", major: "Mechanical Engineering", work: "Mechanical design, analysis", hobbies: "Frisbee, music", diningHall: "Hamp", linkedInIcon: icon("https://www.linkedin.com/in/kyle-o-connell-763273175/", linkedInIcon), gitHubIcon: icon("https://github.com/kyleo9918", gitHubIcon)},
+    {name: "Andy Gutierrez", image: "/Profiles/GutiPic.png", major: "Computer Engineering", work: "Electronics, programming, website design", hobbies: "Piano, guitar, squash", diningHall: "Berk", linkedInIcon: icon("https://www.linkedin.com/in/andres-gutierrez-8758581a2/", linkedInIcon), gitHubIcon: icon("https://github.com/Andy-Guti", gitHubIcon)},
+    {name: "Dillan Wilson", image: "/Profiles/DillanPicture.png", major: "Mechanical Engineering", work: "Chassis design", hobbies: "Running, hockey, table tennis", diningHall: "Hamp", linkedInIcon: icon("https://www.linkedin.com/in/dillan-wilson-2a2116175/", linkedInIcon), gitHubIcon: icon("https://github.com/dillan-wilson", gitHubIcon)},
+    {name: "Emma Thompson", image: "/Profiles/EmmaPic.png", major: "Journalism and Biology", work: "Social media correspondant", hobbies: "Writing, running, drinking coffee", diningHall: "Berk", linkedInIcon: icon("https://www.linkedin.com/in/emma-thompson-5b51b5176", linkedInIcon)}
+  ]
+
   return (
     <body style={{backgroundColor: "#ffffff", margin: 0}}>
       <MuiThemeProvider theme={theme}>
@@ -90,7 +97,7 @@ export default function Team() {
           <Grid container spacing={spacing}>
             {execs.map((person) => (
             <Grid item xs={xSmall} sm={small} md={medium}>
-                <MediaCard name={person.name} image={person.image} execRole={person.execRole} major={person.major} work={person.work} hobbies={person.hobbies} diningHall={person.diningHall} linkedIn={person.linkedIn} gitHub={person.gitHub}/>
+                <MediaCard name={person.name} image={person.image} execRole={person.execRole} major={person.major} work={person.work} hobbies={person.hobbies} diningHall={person.diningHall} linkedInIcon={person.linkedInIcon} gitHubIcon={person.gitHubIcon}/>
             </Grid>))}
           </Grid>
           <Box mt={8}>
@@ -106,7 +113,7 @@ export default function Team() {
           <Grid container spacing={spacing}>
             {members.map((person) => (
             <Grid item xs={xSmall} sm={small} md={medium}>
-                <MediaCard name={person.name} image={person.image} major={person.major} work={person.work} hobbies={person.hobbies} diningHall={person.diningHall} linkedIn={person.linkedIn} gitHub={person.gitHub}/>
+                <MediaCard name={person.name} image={person.image} major={person.major} work={person.work} hobbies={person.hobbies} diningHall={person.diningHall} linkedInIcon={person.linkedInIcon} linkedInIcon={person.linkedInIcon} gitHubIcon={person.gitHubIcon}/>
             </Grid>))}
           </Grid>
         </Box>
