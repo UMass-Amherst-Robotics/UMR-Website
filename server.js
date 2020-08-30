68d76e6c6dbaf93523f2f2f2b8d767670ffa95d1
sdubois18 <samdubois18@icloud.com>
2020-08-30 11:07:43 -0400

    Minor modifications to launch production server

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
-const express = require('express');
-const next = require('next');
-const port = process.env.PORT || 8080;
+// server.js
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
-const handle = app.getRequestHandler();
+const handle = app.getRequestHandler()
 
-app.prepare()
-    .then(() => {
-        const server = express();
-        server.get('*', (req, res) => {
-            return handle(req, res);
-        });
-        
-        server.listen(port, (err) => {
-            if (err) throw err;
-            console.log(`> Ready on http://localhost:${port}`);
-        });
-    })
-    .catch((ex) => {
-        console.error(ex.stack);
-        process.exit(1);
-    });
+app.prepare().then(() => {
+  createServer((req, res) => {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true)
+    const { pathname, query } = parsedUrl
+
+    if (pathname === '/a') {
+      app.render(req, res, '/a', query)
+    } else if (pathname === '/b') {
+      app.render(req, res, '/b', query)
+    } else {
+      handle(req, res, parsedUrl)
+    }
+  }).listen(8080, (err) => {
+    if (err) throw err
+    console.log('> Ready on http://localhost:8080')
+  })
+})
