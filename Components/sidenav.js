17c515903060ffb252d24fe85eaaebe505e6c463
Michael Brauninger <mbrauninger@umass.edu>
2021-02-02 13:04:59 -0500

    Added apply links to navigation

--- Components/sidenav.js
+++ Components/sidenav.js
@@ -85,7 +85,7 @@ export default function TemporaryDrawer() {
         <ListItemLink button key={'Contact'} href='contact'><ListItemIcon><MailIcon /></ListItemIcon><ListItemText primary={'Contact'} /></ListItemLink>
         <ListItemLink button key={'Sponsors'} href='sponsors'><ListItemIcon><PeopleIcon /></ListItemIcon><ListItemText primary={'Sponsors'} /></ListItemLink>
         <ListItemLink button key={'Donate'} href='https://www.gofundme.com/f/umass-robotics?utm_source=customer&utm_medium=copy_link&utm_campaign=p_cf+share-flow-1' target='_blank'><ListItemIcon><AttachMoneyIcon /></ListItemIcon><ListItemText primary={'Donate'} /></ListItemLink>
-        {/*<ListItemLink button key={'Apply'} href='apply'><ListItemIcon><InboxIcon /></ListItemIcon><ListItemText primary={'Apply'} /></ListItemLink>*/}
+        <ListItemLink button key={'Apply'} href='apply'><ListItemIcon><InboxIcon /></ListItemIcon><ListItemText primary={'Apply'} /></ListItemLink>
       </List>
     </div>
   );
